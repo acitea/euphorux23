db59a8aba9c2f3268855001f647c57d0c00f1b73
A. Darryl <akeeladarryl@gmail.com>
2023-05-31 22:33:39 +0800

    hm

--- src/views/ScoreBoard.vue
+++ src/views/ScoreBoard.vue
@@ -42,7 +42,7 @@ export default {
         }
     },
 
-    async mounted() {
+    async beforeMount() {
         const res = await axios.get(process.env.VUE_APP_API_NAME+"/teams");
         this.teams = res.data;
 
